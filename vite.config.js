fc20b46863a12feb34e43bcdc1eb6c1fbd90069a
Douglas <douglas.moore@brightmls.com>
2025-11-21 09:26:15 -0500

    Updates to actions.

--- vite.config.js
+++ vite.config.js
@@ -17,7 +17,7 @@ export default defineConfig({
       },
     },
   },
-  plugins: [dts({ rollupTypes: true })],
+  plugins: [dts({ bundleTypes: true, rollupTypes: true })],
   resolve: {
     alias: hq.get('rollup'),
   },
